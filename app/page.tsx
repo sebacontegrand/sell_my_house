f7bb11a49e544611b5500ceab4363f0d9eb69951
Sebastian Conte-Grand <sebacontegrand@gmail.com>
2024-06-13 19:47:42 -0300

    fix prelistingid6

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,15 @@ const DashBoardPage = async () => {
   const session = await auth();
   if (!session) {
     redirect("/api/auth/signin");
+  } else {
+    redirect("/dashboard");
   }
   return (
     <div className="bg-gray-700 text-blue-500 grid-cols-1 gap-6 sm:grid-cols-2 ">
       <WidgetItem title={"user connected"}>
         <div className="flex flex-col">
-          <span>{session.user?.name}</span>
-          <span>{session.user?.email}</span>
+          <span>User: {session?.user?.name}</span>
+          <span>Email:{session?.user?.email}</span>
         </div>
       </WidgetItem>
     </div>
