3c79ad3bd97b18f3ae7176bfdfb982cc3fab4a75
Sebastian Conte-Grand <sebacontegrand@gmail.com>
2024-06-14 15:08:39 -0300

    fix21

--- next.config.mjs
+++ next.config.mjs
@@ -22,13 +22,7 @@ const nextConfig = {
         }]
     },
 
-    env: {
-        POSTGRES_URL: process.env.POSTGRES_URL,
-        AUTH_SECRET: process.env.AUTH_SECRET,
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
-        AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET
-    }
+
 }
     ;
 
