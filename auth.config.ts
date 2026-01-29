7b59e042eeb931b22160ea47fc9863de91f35f96
Sebastian Conte-Grand <sebastianconte-grand@mac.lan>
2026-01-29 13:49:55 -0300

    fix: remove GitHub provider causing config error

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,6 @@
-import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 import type { NextAuthConfig } from "next-auth"
 
 export default {
-    providers: [Google, GitHub],
+    providers: [Google],
 } satisfies NextAuthConfig
