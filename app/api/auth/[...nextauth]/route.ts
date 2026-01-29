c214c44ab09ba129083cce97f81a4ba690a4a167
Sebastian Conte-Grand <sebastianconte-grand@mac.lan>
2026-01-29 14:31:20 -0300

    fix5

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,3 +3,4 @@ export const dynamic = 'force-dynamic';
 import { handlers } from "@/auth" // Referring to the auth.ts we just created
 
 export const { GET, POST } = handlers
+export const runtime = "nodejs"
