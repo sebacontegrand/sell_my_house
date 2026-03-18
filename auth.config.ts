4fc9da0aa3cb77f0df250046d1f26eaac68fa1df
Sebastian Conte-Grand <sebacontegrand@gmail.com>
2026-03-18 00:29:11 -0300

    config

--- auth.config.ts
+++ auth.config.ts
@@ -2,5 +2,10 @@ import Google from "next-auth/providers/google"
 import type { NextAuthConfig } from "next-auth"
 
 export default {
-    providers: [Google({})],
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+        })
+    ],
 } satisfies NextAuthConfig
